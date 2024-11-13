e77c744c44baa521a92725eb89878a30da88292d
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-11-13 15:14:24 +0000

    Updating the pod on a get

--- cluster.go
+++ cluster.go
@@ -87,6 +87,13 @@ func (t *eventTask) Run() {
 			return
 		}
 
+		updatedPodDetails, err := t.a.client.CoreV1().Pods(t.a.namespace).Get(t.a.ctx, pod.Name, metav1.GetOptions{})
+		if err != nil {
+			l.Error("Error getting pod details", slog.String(loggingKeyError, err.Error()))
+			return
+		}
+		pod = updatedPodDetails
+
 		// If the pod is not running, re-schedule the task
 		if pod.Status.Phase != core.PodRunning {
 			l.Debug("Pod is not running, re-scheduling task")
