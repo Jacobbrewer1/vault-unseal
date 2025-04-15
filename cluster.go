d34042baca14603c7830adc5d61efce6d800e24f
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-04-15 13:34:41 +0100

    fix(unsealing): correcting the target to unseal the vault with (#51)
    * correcting running filter to be pending
    
    * checking if the pod is sealed over the state of the pod
    
    * adding more info to the logs
    
    * metrics annotation
    
    * adding update func
    
    * correcting the ip passed to the generation of the addr
    
    * correcting the ip passed to the generation of the addr
    
    * correcting logging structure
    
    * removing redundant logs

--- cluster.go
+++ cluster.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"log/slog"
+	"strconv"
 
-	"github.com/google/uuid"
 	"github.com/jacobbrewer1/web"
 	"github.com/jacobbrewer1/web/cache"
 	"github.com/jacobbrewer1/web/logging"
@@ -20,6 +20,12 @@ func (a *App) watchNewPods(l *slog.Logger) web.AsyncTaskFunc {
 			AddFunc: newPodHandler(
 				ctx,
 				logging.LoggerWithComponent(l, "new-pod-handler"),
+				a.base.ServiceEndpointHashBucket(),
+				a.config.unsealKeys,
+			),
+			UpdateFunc: updatePodHandler(
+				ctx,
+				logging.LoggerWithComponent(l, "update-pod-handler"),
 				a.base.ServiceEndpointHashBucket(),
 				a.config.unsealKeys,
 			),
@@ -42,42 +48,98 @@ func (a *App) watchNewPods(l *slog.Logger) web.AsyncTaskFunc {
 
 func newPodHandler(ctx context.Context, l *slog.Logger, hashBucket cache.HashBucket, unsealKeys []string) func(any) {
 	return func(podObj any) {
-		l = l.With(
-			slog.String(loggingKeyTaskID, uuid.New().String()),
-			slog.String(loggingKeyEventType, "pod"),
-		)
-
 		pod, ok := podObj.(*core.Pod)
 		if !ok {
-			l.Warn("Object is not a pod")
 			return
 		}
 
+		l = l.With(
+			slog.String(loggingKeyPod, pod.Name),
+		)
+
 		if pod.GetNamespace() != targetNamespace {
-			l.Debug("Pod is not in the target namespace, ignoring")
 			return
 		}
 
 		if !hashBucket.InBucket(pod.Name) {
-			l.Debug("Pod is not in the target hash bucket, ignoring")
 			return
 		}
 
-		if pod.Status.Phase != core.PodRunning {
-			l.Debug("Pod is not running, re-scheduling task")
+		if !isVaultPod(pod) {
+			return
+		}
+		if !isVaultPodSealed(pod) {
 			return
 		}
 
-		l.Info("New pod added, attempting to unseal vault", slog.String(loggingKeyPod, pod.Name))
+		l.Info("Updated Vault pod detected, attempting to unseal vault", slog.String(loggingKeyPod, pod.Name))
 
 		if err := unsealNewVaultPod( // nolint:revive // Traditional error handling
 			ctx,
 			l,
-			generateVaultAddress(pod.Spec.Containers[0].Ports, pod.Status.HostIP),
+			generateVaultAddress(pod.Spec.Containers[0].Ports, pod.Status.PodIP),
 			unsealKeys,
 		); err != nil {
 			l.Error("Error unsealing vault", slog.String(loggingKeyError, err.Error()))
 			return
 		}
 	}
+}
+
+func updatePodHandler(ctx context.Context, l *slog.Logger, hashBucket cache.HashBucket, unsealKeys []string) func(any, any) {
+	return func(_, newObj any) {
+		pod, ok := newObj.(*core.Pod)
+		if !ok {
+			return
+		}
+
+		l = l.With(
+			slog.String(loggingKeyPod, pod.Name),
+		)
+
+		if pod.GetNamespace() != targetNamespace {
+			return
+		}
+
+		if !hashBucket.InBucket(pod.Name) {
+			return
+		}
+
+		if !isVaultPod(pod) {
+			return
+		}
+		if !isVaultPodSealed(pod) {
+			return
+		}
+
+		l.Info("Updated Vault pod detected, attempting to unseal vault", slog.String(loggingKeyPod, pod.Name))
+
+		if err := unsealNewVaultPod( // nolint:revive // Traditional error handling
+			ctx,
+			l,
+			generateVaultAddress(pod.Spec.Containers[0].Ports, pod.Status.PodIP),
+			unsealKeys,
+		); err != nil {
+			l.Error("Error unsealing vault", slog.String(loggingKeyError, err.Error()))
+			return
+		}
+	}
+}
+
+func isVaultPod(pod *core.Pod) bool {
+	return pod.Labels["app.kubernetes.io/name"] == "vault"
+}
+
+func isVaultPodSealed(pod *core.Pod) bool {
+	sealed, ok := pod.Labels["vault-sealed"]
+	if !ok {
+		return false
+	}
+
+	isSealed, err := strconv.ParseBool(sealed)
+	if err != nil {
+		return false
+	}
+
+	return isSealed
 }
--- consts.go
+++ consts.go
@@ -3,12 +3,10 @@ package main
 const (
 	appName = "vault-unseal"
 
-	loggingKeyError     = "err"
-	loggingKeyPod       = "pod"
-	loggingKeyTaskID    = "task_id"
-	loggingKeyEventType = "event_type"
-	loggingKeySealed    = "sealed"
-	loggingKeyProgress  = "progress"
+	loggingKeyError    = "err"
+	loggingKeyPod      = "pod"
+	loggingKeySealed   = "sealed"
+	loggingKeyProgress = "progress"
 
 	targetNamespace = "vault"
 )
